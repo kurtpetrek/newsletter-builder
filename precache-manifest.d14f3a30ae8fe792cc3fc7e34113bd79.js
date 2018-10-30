65ad45183014eca2f9aa01fdb70dbc4d0e00b356
kpuipath <kurtpetrek@gmail.com>
2018-10-29 21:40:28 -0400

    Updates

---
+++ precache-manifest.d14f3a30ae8fe792cc3fc7e34113bd79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d73424a0f68a1b4080",
+    "url": "/newsletter-builder/static/js/runtime~main.84d73424.js"
+  },
+  {
+    "revision": "5ba0b4386089b4ac1138",
+    "url": "/newsletter-builder/static/js/main.5ba0b438.chunk.js"
+  },
+  {
+    "revision": "bc8821abdf4bf160dd36",
+    "url": "/newsletter-builder/static/js/1.bc8821ab.chunk.js"
+  },
+  {
+    "revision": "5ba0b4386089b4ac1138",
+    "url": "/newsletter-builder/static/css/main.c23bb7c2.chunk.css"
+  },
+  {
+    "revision": "b03e6d4b73404233b4dd384c221d0406",
+    "url": "/newsletter-builder/index.html"
+  }
+];
